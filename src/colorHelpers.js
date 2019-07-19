a2f5008234434e66ac6c0b3eb422edc7521038b6
Zachary Bowman <zchrybwmn@gmail.com>
2019-07-19 11:28:32 -0400

    Add Navbar Component, Refactor Navbar Code in Component File, Font, Styling

--- src/colorHelpers.js
+++ src/colorHelpers.js
@@ -22,6 +22,7 @@ function generateFullPalette(starterPalette) {
                 name: `${color.name} ${levels[i]}`,
                 id: color.name.toLowerCase().replace(/ /g, "-"),
                 hex: scale[i],
+                // returns a rgba value instead of hex
                 rgb: chroma(scale[i]).css(),
                 rgba: chroma(scale[i]).css().replace("rgb", "rgba").replace(")", ",1.0)")
             })
@@ -34,16 +35,23 @@ function generateFullPalette(starterPalette) {
 function getRange(hexColor) {
     const end = "#fff";
     return [
+        // darkens the hex color by 1.4 ratio, i.e. "dark blue"
         chroma(hexColor).darken(1.4).hex(),
+        // blue
         hexColor,
+        // white
         end
     ]
 }
 function getScale(hexColor, numberOfColorsToGenerate) {
     return chroma
+        // returns the full gradient (i.e. smooth gradient from dark blue, to blue, to white)
         .scale(getRange(hexColor))
+        // changes mode of color mixing to help decrease grayness in the gradient
         .mode("lab")
+        // grabs equidistant colors in the scale made above in a given amount
         .colors(numberOfColorsToGenerate);
+        // finishes with X amount of colors equidistant
 };
 
 export {generateFullPalette};
