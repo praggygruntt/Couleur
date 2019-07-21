3a8215e42cad99eefb726574de75edc92fa1b5bf
Zachary Bowman <zchrybwmn@gmail.com>
2019-07-21 14:35:26 -0400

    Install react-router-dom, Begin Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import Palette from './Components/Palette';
 import seedPalettes from './seedPalettes';
 import {generateFullPalette} from './colorHelpers';
 
 function App() {
   return (
-    <div>
-      {/* generates a Palette using only the first element of the seedPalettes array */}
-      <Palette palette={generateFullPalette(seedPalettes[8])} />
-    </div>
+    <Switch>
+      <Route exact path="/" render={() => <h1>Palette List Goes Here</h1>}/>
+      <Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>}/>
+    </Switch>
+    // <div>
+    //   <Palette palette={generateFullPalette(seedPalettes[8])} />
+    // </div>
   );
 }
 
