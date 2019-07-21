a0ee9fe351bfbd8a3b53d6bebb04f88dab884d1e
Zachary Bowman <zchrybwmn@gmail.com>
2019-07-21 16:10:17 -0400

    Style MiniPalette More

--- src/Components/PaletteList.js
+++ src/Components/PaletteList.js
@@ -6,7 +6,7 @@ import { withStyles } from '@material-ui/styles';
 const styles = {
     root: {
         background: "pink",
-        height: "100%",
+        height: "100vh",
         alignItems: 'flex-start',
         justifyContent: 'center',
         display: "flex"
